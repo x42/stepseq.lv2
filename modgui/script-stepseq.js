71c94139904fb5b4843c43dc6327df52cf867ae8
Robin Gareus <robin@gareus.org>
2016-08-13 23:43:58 +0200

    renice JS button bind approach

--- modgui/script-stepseq.js
+++ modgui/script-stepseq.js
@@ -1,23 +1,27 @@
-x42_seq_setdisplaymode = function (me, id, mode) {
-	$("div.x42-stepseq" + id).find ("[x42-role=seq-radio-dpy]").removeClass ("selected");
-	$(me).addClass ("selected");
-	switch (mode) {
-		case 'drum':
-			$("div.x42-stepseq" + id).find ("[x42-role=seq-note]").addClass ("drum");
-			$("div.x42-stepseq" + id).find ("[x42-role=seq-note]").removeClass ("note");
-			$("div.x42-stepseq" + id).find ("[x42-role=seq-note]").removeClass ("nums");
-			break;
-		case 'nums':
-			$("div.x42-stepseq" + id).find ("[x42-role=seq-note]").removeClass ("drum");
-			$("div.x42-stepseq" + id).find ("[x42-role=seq-note]").addClass ("note");
-			$("div.x42-stepseq" + id).find ("[x42-role=seq-note]").removeClass ("nums");
-			break;
-		case 'note':
-			$("div.x42-stepseq" + id).find ("[x42-role=seq-note]").removeClass ("drum");
-			$("div.x42-stepseq" + id).find ("[x42-role=seq-note]").removeClass ("note");
-			$("div.x42-stepseq" + id).find ("[x42-role=seq-note]").addClass ("nums");
-			break;
-		default:
-			break;
-	}
+function (event) {
+
+	event.icon.find("[x42-role=seq-radio-nums]").click(function(){
+		event.icon.find ("div.displayradio").removeClass ("selected");
+		$(this).addClass ("selected");
+		event.icon.find ("[x42-role=seq-note]").removeClass ("drum");
+		event.icon.find ("[x42-role=seq-note]").removeClass ("note");
+		event.icon.find ("[x42-role=seq-note]").addClass ("nums");
+	});
+
+	event.icon.find("[x42-role=seq-radio-note]").click(function(){
+		event.icon.find ("div.displayradio").removeClass ("selected");
+		$(this).addClass ("selected");
+		event.icon.find ("[x42-role=seq-note]").removeClass ("drum");
+		event.icon.find ("[x42-role=seq-note]").removeClass ("nums");
+		event.icon.find ("[x42-role=seq-note]").addClass ("note");
+	});
+
+	event.icon.find("[x42-role=seq-radio-drum]").click(function(){
+		event.icon.find ("div.displayradio").removeClass ("selected");
+		$(this).addClass ("selected");
+		event.icon.find ("[x42-role=seq-note]").removeClass ("note");
+		event.icon.find ("[x42-role=seq-note]").removeClass ("nums");
+		event.icon.find ("[x42-role=seq-note]").addClass ("drum");
+	});
+
 }
