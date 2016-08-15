7c6d42dff73ce9fd3b8fc27f0c74987c4844cae7
Robin Gareus <robin@gareus.org>
2016-08-15 13:13:32 +0200

    update JS API to use upcoming MOD API.

--- modgui/script-stepseq.js
+++ modgui/script-stepseq.js
@@ -1,4 +1,18 @@
-function (event) {
+function (event, funcs) {
+	if (event.type != "start") {
+		return;
+	}
+
+	function resetGridValue() {
+		if (event.api_version >= 1) {
+			funcs.set_port_value($(this).attr('mod-port-symbol'), 0);
+		} else {
+			/* this is for MOD v1.0 backwards compatibility and prototyping.
+			 * DO NOT USE THIS APPROACH IN NEW CODE. It bypasses
+			 * checks for bound controls among other things. */
+			$(this).controlWidget('setValue', 0);
+		}
+	}
 
 	event.icon.find("[x42-role=seq-radio-nums]").click(function(){
 		event.icon.find ("div.displayradio").removeClass ("selected");
@@ -26,15 +40,15 @@ function (event) {
 
 	event.icon.find("div.resetbutton.col").click(function(){
 		var c = $(this).attr('grid-col');
-		event.icon.find("[mod-role=input-control-port][grid-col="+c+"]").controlWidget('setValue', 0);
+		event.icon.find("[mod-role=input-control-port][grid-col="+c+"]").each(resetGridValue);
 	});
 
 	event.icon.find("div.resetbutton.row").click(function(){
 		var r = $(this).attr('grid-row');
-		event.icon.find("[mod-role=input-control-port][grid-row="+r+"]").controlWidget('setValue', 0);
+		event.icon.find("[mod-role=input-control-port][grid-row="+r+"]").each(resetGridValue);
 	});
 
 	event.icon.find("div.resetbutton.all").click(function(){
-		event.icon.find("[mod-role=input-control-port][grid-row]").controlWidget('setValue', 0);
+		event.icon.find("[mod-role=input-control-port][grid-row]").each(resetGridValue);
 	});
 }
